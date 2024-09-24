cce92c8d282d816fbc6d159ba8fdc9c7a398b32d
Khrubilov Daniil <khrubilov.d@promail366.com>
2024-09-24 20:10:59 +0300

    Solve weather-app

--- 01-basics/20-weather/WeatherApp.js
+++ 01-basics/20-weather/WeatherApp.js
@@ -9,43 +9,52 @@ export default defineComponent({
       <h1 class="title">Погода в Средиземье</h1>
 
       <ul class="weather-list unstyled-list">
-        <li class="weather-card weather-card--night">
-          <div class="weather-alert">
+        
+        <li v-for="city in cityList" class="weather-card" :class="{'weather-card--night': city.current.dt < city.current.sunrise || city.current.dt > city.current.sunset }">
+          <div v-if="city.alert" class="weather-alert">
             <span class="weather-alert__icon">⚠️</span>
-            <span class="weather-alert__description">Королевская метеослужба короля Арагорна II: Предвещается наступление сильного шторма.</span>
+            <span class="weather-alert__description">{{ city.alert.sender_name + ': ' + city.alert.description }}</span>
           </div>
           <div>
             <h2 class="weather-card__name">
-              Гондор
+              {{ city.geographic_name }}
             </h2>
             <div class="weather-card__time">
-              07:17
+              {{ city.current.dt }}
             </div>
           </div>
           <div class="weather-conditions">
-            <div class="weather-conditions__icon" title="thunderstorm with heavy rain">⛈️</div>
-            <div class="weather-conditions__temp">15.0 °C</div>
+            <div class="weather-conditions__icon" :title="city.current.weather.description">{{ WeatherConditionIcons[city.current.weather.id] }}</div>
+            <div class="weather-conditions__temp">{{ city.current.temp }} °C</div>
           </div>
           <div class="weather-details">
             <div class="weather-details__item">
               <div class="weather-details__item-label">Давление, мм рт. ст.</div>
-              <div class="weather-details__item-value">754</div>
+              <div class="weather-details__item-value">{{ city.current.pressure }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Влажность, %</div>
-              <div class="weather-details__item-value">90</div>
+              <div class="weather-details__item-value">{{ city.current.humidity }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Облачность, %</div>
-              <div class="weather-details__item-value">100</div>
+              <div class="weather-details__item-value">{{ city.current.clouds }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Ветер, м/с</div>
-              <div class="weather-details__item-value">10.5</div>
+              <div class="weather-details__item-value">{{ city.current.wind_speed }}</div>
             </div>
           </div>
         </li>
       </ul>
     </div>
   `,
+
+  setup() {
+    const cityList = getWeatherData();
+    return {
+      cityList,
+      WeatherConditionIcons,
+    }
+  }
 })
